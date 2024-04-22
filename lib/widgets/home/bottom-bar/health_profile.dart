c0a03558912fcca860d475cc118021ab00c97631
Harsha Kumar <ahkharsha@gmail.com>
2024-04-22 21:52:57 +0530

    fix health_profile.dart

--- lib/widgets/home/bottom-bar/health_profile.dart
+++ lib/widgets/home/bottom-bar/health_profile.dart
@@ -1,13 +1,9 @@
-import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:pregathi/bottom-sheet/insta_share_bottom_sheet.dart';
-import 'package:pregathi/const/constants.dart';
 import 'package:pregathi/navigators.dart';
 import 'package:sizer/sizer.dart';
-import 'package:tflite_flutter/tflite_flutter.dart';
 
 class HealthProfileScreen extends StatefulWidget {
   const HealthProfileScreen({Key? key}) : super(key: key);
@@ -17,49 +13,17 @@ class HealthProfileScreen extends StatefulWidget {
 }
 
 class _HealthProfileScreenState extends State<HealthProfileScreen> {
-  String predValue='Predicting...';
-  final ref = FirebaseDatabase(
-          databaseURL:
-              "https://pregathi-69-default-rtdb.asia-southeast1.firebasedatabase.app")
-      .ref('sensors');
-  late String bpm;
-  late String sValue;
   final TextEditingController _ageController = TextEditingController();
-  final TextEditingController _gestationalAgeController =
-      TextEditingController();
+  final TextEditingController _gestationalAgeController = TextEditingController();
   final TextEditingController _bmiController = TextEditingController();
-  final TextEditingController _sympatheticCamController =
-      TextEditingController();
-  final TextEditingController _parasympatheticCamController =
-      TextEditingController();
-  bool isSaving = false;
+  final TextEditingController _sympatheticCamController = TextEditingController();
+  final TextEditingController _parasympatheticCamController = TextEditingController();
   final User? user = FirebaseAuth.instance.currentUser;
 
   @override
   void initState() {
     super.initState();
     loadData();
-    predData();
-  }
-
-  Future<void> predData() async {
-    print('Execution starting...');
-    await runPrediction();
-    print('Execution complete...');
-  }
-
-  runPrediction() async {
-    final interpreter =
-        await Interpreter.fromAsset('labour_pain_prediction_model.tflite');
-    var input = [
-      [24, 24, 24, 0.67, 0.34, 95]
-    ];
-    var output = List.filled(1, 0).reshape([1, 1]);
-    interpreter.run(input, output);
-     print(output[0][0]);
-    setState(() {
-      predValue = output[0][0].toString();
-    });
   }
 
   Future<void> loadData() async {
@@ -84,77 +48,58 @@ class _HealthProfileScreenState extends State<HealthProfileScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(
-            icon: Icon(
-              Icons.arrow_back_ios_new_rounded,
-              color: Colors.white,
-            ),
-            onPressed: () => goBack(context)),
-        title: Text(
-          'Health Profile',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-          ),
-        ),
-        centerTitle: false,
-        backgroundColor: primaryColor,
+        title: Text('Health Profile'),
+        centerTitle: true,
         actions: [
-          TextButton(
+          IconButton(
             onPressed: () {
               _updateHealthProfile();
             },
-            child: Text(
-              'Save',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 15.sp,
-              ),
-            ),
+            icon: Icon(Icons.save),
           ),
         ],
       ),
-      floatingActionButton: Padding(
-        padding: const EdgeInsets.only(bottom: 10.0, right: 10.0),
-        child: FloatingActionButton(
-          onPressed: () {
-            showModalBottomSheet<void>(
-              context: context,
-              builder: (BuildContext context) {
-                return InstaShareBottomSheet();
-              },
-            );
-          },
-          backgroundColor: Colors.red,
-          foregroundColor: boxColor,
-          highlightElevation: 50,
-          child: Icon(
-            Icons.warning_outlined,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(20.sp),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              TextFormField(
+                controller: _ageController,
+                decoration: InputDecoration(labelText: 'Age'),
+                keyboardType: TextInputType.number,
+              ),
+              TextFormField(
+                controller: _gestationalAgeController,
+                decoration: InputDecoration(labelText: 'Gestational Age'),
+                keyboardType: TextInputType.number,
+              ),
+              TextFormField(
+                controller: _bmiController,
+                decoration: InputDecoration(labelText: 'BMI'),
+                keyboardType: TextInputType.number,
+              ),
+              TextFormField(
+                controller: _sympatheticCamController,
+                decoration: InputDecoration(labelText: 'Sympathetic CAM (LF Form)'),
+              ),
+              TextFormField(
+                controller: _parasympatheticCamController,
+                decoration: InputDecoration(labelText: 'Parasympathetic CAM (HF Form)'),
+              ),
+            ],
           ),
         ),
       ),
-      body: isSaving
-          ? progressIndicator(context)
-          : SingleChildScrollView(
-              child: Padding(
-                padding: EdgeInsets.all(20.sp),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text('Pred Value: $predValue'),
-                  ],
-                ),
-              ),
-            ),
     );
   }
 
   Future<void> _updateHealthProfile() async {
     try {
       final User? user = FirebaseAuth.instance.currentUser;
-      setState(() {
-        isSaving = true;
-      });
+
+      // Update health profile information
       await FirebaseFirestore.instance
           .collection('users')
           .doc(user!.uid)
@@ -166,11 +111,7 @@ class _HealthProfileScreenState extends State<HealthProfileScreen> {
         'parasympatheticCam': _parasympatheticCamController.text,
       });
 
-      dialogueBoxWithButton(context, 'Health Profile updated successfully');
-
-      setState(() {
-        isSaving = false;
-      });
+      Fluttertoast.showToast(msg: 'Health profile updated successfully');
     } catch (e) {
       Fluttertoast.showToast(msg: 'Failed to update health profile');
     }
